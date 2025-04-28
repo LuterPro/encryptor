00344dda45242da94377742f96447497885f727e
Dmitry Prohorof <luter_86@mail.ru>
2025-04-28 22:29:12 +0300

    Upgrade main menu

--- src/runapplication/RunApplication.java
+++ src/runapplication/RunApplication.java
@@ -2,8 +2,6 @@ package runapplication;
 
 import navigations.NavigationMenu;
 
-import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class RunApplication {
